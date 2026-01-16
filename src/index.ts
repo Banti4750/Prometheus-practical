aa1179801a5649d7a863811667e38bf00281075a
Banti4750 <bantikumar6203818460@gmail.com>
2026-01-16 18:08:46 +0530

    adding prom-client for counters

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import express from "express";
-import { middleware } from "./middleware";
+import { NextFunction, Request, Response } from "express";
+import client from "prom-client";
+
+// Create a counter metric
+const requestCounter = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status_code']
+});
+
+
+const requestCountMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+        const endTime = Date.now();
+        console.log(`Request took ${endTime - startTime}ms`);
+
+        // Increment request counter
+        requestCounter.inc({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            status_code: res.statusCode
+        });
+    });
+
+    next();
+};
 
 const app = express();
 
 app.use(express.json());
-app.use(middleware);
+app.use(requestCountMiddleware);
 app.get("/user", (req, res) => {
     res.send({
         name: "John Doe",
@@ -19,5 +46,11 @@ app.post("/user", (req, res) => {
         id: 1,
     });
 });
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
 
 app.listen(3000);
